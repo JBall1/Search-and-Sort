4ad26a5628c3a71d504bbb96b4768e95727996fd
Joshua Ball <joshua.ball@ttu.edu>
2018-12-04 19:11:44 -0600

    Update color.v

--- color.v
+++ color.v
@@ -1,24 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09/05/2018 01:48:05 PM
-// Design Name: 
-// Module Name: color
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
+//Main color sensor module for TCS3200.
 
 module color(
     output reg Selector1, // S2
